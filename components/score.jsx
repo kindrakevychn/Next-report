c2bec0c04a5312abe899c6ed36414ec3eb7f1f5e
kindrakevychn <kindrakevychn@gmail.com>
2022-02-07 10:32:49 +0300

    Refactoring of score.jsx

--- components/score.jsx
+++ components/score.jsx
@@ -4,7 +4,7 @@ import styles from './score.module.css';
 
 export default function Score({score}) {
     let status = scoreToStatus(score);
-    let title = '';
+    let description = '';
 
     // Width of text is not persistent, but we need element width
     // to calculate its left margin. We will use approximate width
@@ -14,43 +14,16 @@ export default function Score({score}) {
 
     if (score < 35) {
         statusWidth = 0.9;
-        title = (
-            `"Bad" means our model analyzed this article and found that it\n` +
-            "is nearly devoid of any informative value. Scores in this range\n" +
-            "are typical in fake/satire news outlets, marketing or advertising\n" +
-            `copy, pure propaganda, or conspiracy theorists' websites.\n` +
-            "Our model has no way to distinguish satire from intentionally\n" +
-            "misleading materials, and therefore penalizes both to the same\n" +
-            "degree (based on text alone)."
-        );
+        description = DESCRIPTION.bad;
     } else if (score < 65) {
         statusWidth = 2.05;
-        title = (
-            `"Mediocre" means our model analyzed the article and found that it\n` +
-            "mixes some informative elements with A LOT of negative persuasion\n" +
-            "techniques, aiming primarily to persuade (not inform) the reader.\n" +
-            "Scores in this range are typical in partisan or agenda-driven websites,\n" +
-            "and are often seen in some of the subtler propaganda campaigns."
-        );
+        description = DESCRIPTION.mediocre;
     } else if (score < 85) {
         statusWidth = 1.25;
-        title = (
-            `"Good" means our model analyzed the article's text and found that it\n` +
-            "mixes informative elements with persuasive elements, aiming both to\n" +
-            "inform and to sway the reader's opinion in some direction. In other\n" +
-            "words, this article was penalized for using some unnecessary flourishes\n" +
-            "(e.g. subjective language, biased presuppositions, hyperbolic choice of\n" +
-            "words, etc) that a purely-informative article would generally avoid.\n" +
-            "Scores in this range are typical for editorials or opinion articles."
-        );
+        description = DESCRIPTION.good;
     } else {
         statusWidth = 1.4;
-        title = (
-            `"Great" means our model analyzed the article's text and found that it\n` +
-            "is a) informative, and b) does not contain any negative elements that\n" +
-            "attempt to persuade or trick the reader. Scores in this range are\n" +
-            "typical for encyclopedia entries or strictly-factual news reports."
-        );
+        description = DESCRIPTION.great;
     }
 
     let sliderColor = scoreToColor(score);
@@ -74,7 +47,7 @@ export default function Score({score}) {
                 >
                     {'Score'}
                     <HoverInfo
-                        text={title}
+                        text={description}
                     />
                 </div>
 
@@ -170,3 +143,37 @@ function ScaleIcon() {
         </svg>
     );
 }
+
+const DESCRIPTION = {
+    bad: (
+        `"Bad" means our model analyzed this article and found that it\n` +
+        "is nearly devoid of any informative value. Scores in this range\n" +
+        "are typical in fake/satire news outlets, marketing or advertising\n" +
+        `copy, pure propaganda, or conspiracy theorists' websites.\n` +
+        "Our model has no way to distinguish satire from intentionally\n" +
+        "misleading materials, and therefore penalizes both to the same\n" +
+        "degree (based on text alone)."
+    ),
+    mediocre: (
+        `"Mediocre" means our model analyzed the article and found that it\n` +
+        "mixes some informative elements with A LOT of negative persuasion\n" +
+        "techniques, aiming primarily to persuade (not inform) the reader.\n" +
+        "Scores in this range are typical in partisan or agenda-driven websites,\n" +
+        "and are often seen in some of the subtler propaganda campaigns."
+    ),
+    good: (
+        `"Good" means our model analyzed the article's text and found that it\n` +
+        "mixes informative elements with persuasive elements, aiming both to\n" +
+        "inform and to sway the reader's opinion in some direction. In other\n" +
+        "words, this article was penalized for using some unnecessary flourishes\n" +
+        "(e.g. subjective language, biased presuppositions, hyperbolic choice of\n" +
+        "words, etc) that a purely-informative article would generally avoid.\n" +
+        "Scores in this range are typical for editorials or opinion articles."
+    ),
+    great: (
+        `"Great" means our model analyzed the article's text and found that it\n` +
+        "is a) informative, and b) does not contain any negative elements that\n" +
+        "attempt to persuade or trick the reader. Scores in this range are\n" +
+        "typical for encyclopedia entries or strictly-factual news reports."
+    )
+};
